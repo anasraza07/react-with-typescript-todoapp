8272e50f1acb7cc342d77b0276752f50a3907418
Anasraza <ar470684@gmail.com>
2025-12-03 23:46:42 +0500

    added react icons and styled the todoapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { use, useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import { IoMdAdd } from "react-icons/io";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
 
 const App = () => {
   const [todoInput, setTodoInput] = useState("");
   const [updatedTodoInput, setUpdatedTodoInput] = useState("");
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([{ id: "1", todo: "go to gym" },
+  { id: "2", todo: "eat lunch" }
+  ]);
   const [editId, setEditId] = useState("");
 
   interface Todo {
@@ -45,33 +51,37 @@ const App = () => {
   }
 
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1>Todo App!</h1>
-      <input type="text" placeholder="Enter your todo..."
-        onChange={e => setTodoInput(e.target.value)} value={todoInput} />
-      <button onClick={addTodo}>Add</button>
-
-      {/* todos */}
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>
-            {todo.id != editId ? (
-              <>
-                <span>{todo.todo}</span>
-                <button onClick={() => editTodo(todo)}>Edit</button>
-                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-              </>
-            ) : (
-              <>
-                <input type="text" value={updatedTodoInput}
-                  onChange={e => setUpdatedTodoInput(e.target.value)} />
-                <button onClick={() => saveTodo(todo.id, updatedTodoInput)}>Save</button>
-              </>
-            )}
-          </li>
-
-        ))}
-      </ul>
+    <div className="min-h-screen">
+      <div className="container max-w-[600px] mx-auto p-4 bg-gray-50">
+        <h1 className="text-4xl font-bold my-8">Todo App!</h1>
+        <div className="flex gap-3 mb-6">
+          <input type="text" className="flex-1 border border-gray-400 rounded-sm p-1 outline-none px-2" placeholder="Enter your todo..."
+            onChange={e => setTodoInput(e.target.value)} value={todoInput} />
+          <button onClick={addTodo} className="bg-purple-500 text-white px-2 rounded-sm font-extrabold cursor-pointer text-lg border-none outline-none hover:bg-purple-500/90"><IoMdAdd /></button>
+        </div>
+        {/* todos */}
+        <ul className="flex flex-col gap-4">
+          {todos.map(todo => (
+            <li key={todo.id} className="bg-gray-200 p-1 px-2 flex justify-between items-center rounded-sm">
+              {todo.id != editId ? (
+                <>
+                  <span className="text-lg">{todo.todo}</span>
+                  <div className="space-x-2">
+                    <button className="bg-blue-500 hover:bg-blue-500/90 p-2 text-white rounded-sm cursor-pointer" onClick={() => editTodo(todo)}><FaEdit /></button>
+                    <button className="bg-red-500 hover:bg-red-500/90 p-2 text-white rounded-sm cursor-pointer" onClick={() => deleteTodo(todo.id)}><MdDelete /></button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <input type="text" value={updatedTodoInput} className="bg-white rounded-sm px-2"
+                    onChange={e => setUpdatedTodoInput(e.target.value)} />
+                  <button className="bg-purple-500 text-white px-2 rounded-sm font-bold cursor-pointer text-lg" onClick={() => saveTodo(todo.id, updatedTodoInput)}>Save</button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
